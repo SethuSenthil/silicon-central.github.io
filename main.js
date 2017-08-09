c47cb1f4b5ef5de7339ed7c67b4036422e85e0a7
Silicon-Central <Silicon-Central@users.noreply.github.com>
2017-08-09 15:18:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//JS
+//Console Log
+console.log("%c////MESSAGE HERE FOR CONSOLE USERS", "color:#4285F4; font-size:30px; font-family:'Product Sans', Arial, sans-serif;");
+
+//WOW.js config
+wow = new WOW(
+                      {
+                      boxClass:     'wow',
+                      animateClass: 'animated',
+                      offset:       0,
+                      mobile:       true,
+                      live:         true
+                    }
+                    )
+//Use class "nomobile" to disable certian element wow anamation on mobile devices
